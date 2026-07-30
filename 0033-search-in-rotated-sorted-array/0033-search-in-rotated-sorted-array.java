b41d115de55ef1b93e728cb4f0aacf48a36ff966
sai-ganesh-4539 <saiganeshreddy.j@gmail.com>
2026-07-30 15:55:05 +0530

    Time: 0 ms (100%), Space: 44 MB (11.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,12 +1,24 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length;
-        int res = -1;
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                res = i;
+        int l = 0;
+        int r = nums.length - 1;
+        while(l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) return m;
+            if (nums[m] < nums[r]) {
+                if (nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                if (nums[l] <= target && target <= nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
             }
         }
-        return res;
+        return -1;
     }
 }
