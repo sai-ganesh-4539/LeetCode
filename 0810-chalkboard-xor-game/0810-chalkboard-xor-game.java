d45bb3af258237a24be171980ad85258321461ce
sai-ganesh-4539 <saiganeshreddy.j@gmail.com>
2026-08-19 19:45:30 +0530

    Time: 0 ms (100%), Space: 46.6 MB (15.61%) - LeetHub

---
+++ 0810-chalkboard-xor-game/0810-chalkboard-xor-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean xorGame(int[] nums) {
+        int xor = 0;
+        for (int x : nums) {
+            xor ^= x;
+        }
+        return xor == 0 || nums.length % 2 == 0;
+    }
+}
