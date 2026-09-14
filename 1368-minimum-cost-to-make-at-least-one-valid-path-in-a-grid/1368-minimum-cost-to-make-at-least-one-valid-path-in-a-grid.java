2252b36ab0756667ecbc0cf84ff29224f2ecbe99
sai-ganesh-4539 <saiganeshreddy.j@gmail.com>
2026-09-14 23:22:26 +0530

    Time: 13 ms (84.09%), Space: 46.6 MB (97.84%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,7 +1,7 @@
 class Solution {
-    // 0/1 bfs is more optimized than dijkstra here 
-    // O(mn) - 0/1 bfs
-    // O(mnlog(mn)) - dijkstra
+    // 0/1 BFS is more optimized than Dijkstra here
+    // O(mn) - 0/1 BFS
+    // O(mn log(mn)) - Dijkstra
     public int minCost(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -9,26 +9,34 @@ class Solution {
         for (int i = 0; i < m; i++) {
             Arrays.fill(d[i], Integer.MAX_VALUE);
         }
-        int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        int[][] dir = {{0, 1},{0, -1},{1, 0},{-1, 0}};
+        Deque<int[]> dq = new ArrayDeque<>();
         d[0][0] = 0;
-        pq.add(new int[]{0, 0, 0});
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
+        dq.addFirst(new int[]{0, 0});
+        while (!dq.isEmpty()) {
+            int[] curr = dq.pollFirst();
             int r = curr[0];
             int c = curr[1];
-            int cost = curr[2];
-            if (cost > d[r][c]) continue;
             for (int i = 0; i < 4; i++) {
                 int nR = r + dir[i][0];
                 int nC = c + dir[i][1];
-                if (nR < 0 || nR >= m || nC < 0 || nC >= n) continue;
-                int newCost;
-                if (grid[r][c] == i + 1) newCost = cost;
-                else newCost = cost + 1;
+                if (nR < 0 || nR >= m || nC < 0 || nC >= n) {
+                    continue;
+                }
+                int cost;
+                if (grid[r][c] == i + 1) {
+                    cost = 0;
+                } else {
+                    cost = 1;
+                }
+                int newCost = d[r][c] + cost;
                 if (newCost < d[nR][nC]) {
                     d[nR][nC] = newCost;
-                    pq.add(new int[]{nR, nC, newCost});
+                    if (cost == 0) {
+                        dq.addFirst(new int[]{nR, nC});
+                    } else {
+                        dq.addLast(new int[]{nR, nC});
+                    }
                 }
             }
         }
